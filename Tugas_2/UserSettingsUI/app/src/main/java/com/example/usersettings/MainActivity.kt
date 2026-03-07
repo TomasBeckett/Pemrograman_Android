96384127c13240f9f2484ff82b03e8b25502f14c
Tomas Becket <tomas.becket@ti.ukdw.ac.id>
2026-03-07 22:20:21 +0700

    Tugas_2

---
+++ Tugas_2/UserSettingsUI/app/src/main/java/com/example/usersettings/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.usersettings
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinnerCountry)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.country_list,
+            R.layout.spinner_item
+        )
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+}
