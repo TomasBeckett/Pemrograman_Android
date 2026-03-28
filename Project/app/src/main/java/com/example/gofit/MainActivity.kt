a1c11ea060f051fa1fdc39605593ca4e730230e2
Tomas Becket <tomas.becket@ti.ukdw.ac.id>
2026-03-29 01:47:34 +0700

    Tugas_4

---
+++ Project/app/src/main/java/com/example/gofit/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.gofit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottom_navigation)
+
+        // Default fragment
+        loadFragment(HomeFragment())
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_workout -> {
+                    loadFragment(WorkoutFragment())
+                    true
+                }
+                R.id.nav_schedule -> {
+                    loadFragment(ScheduleFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
+    }
+}
